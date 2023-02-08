b2eb9857a831624db4867ba34b62af9cfb9895a4
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-02-08 23:36:31 +0300

    add code snippets

---
+++ input.py
@@ -0,0 +1,4 @@
+name = input("What is your name?")
+name = name.title()
+print()
+print("Name:", name)
---
+++ names.py
@@ -0,0 +1,8 @@
+from faker import Faker
+
+fake = Faker('en_IN')
+for _ in range(10):
+   print(fake.bank())
+
+for _ in range(20):
+   print(fake.first_name(), fake.last_name())   
