79f7c37521d2a1ea5a17a44cf1d651be8917170d
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-02-11 19:40:23 +0300

    todo: add random seed of payments

--- payments/with_class.py
+++ payments/with_class.py
@@ -1,47 +1,60 @@
-class Accounts:
+class CurrentAccountsBase:
+
     def __init__(self):
-        self.balances = {"bank": 0}
+        self.client_balances = {}
+        self.own_account = 0
+
+    @property    
+    def users(self):
+        """List all users with current accounts."""
+        # Implement here - you need to list all keys from self.balances dictionary.
+
+    @property
+    def all_balances(self):
+        return {**self.client_balances, "bank": self.own_account}
 
     def create_user(self, name):
-        # Is this always correct to do?
-        self.balances[name] = 0
+        """Add new a user *name* with zero current account balance."""
+        # Is this always a correct thing to do?
+        self.client_balances[name] = 0
 
     def add_money(self, name, amount):
-        """Deposit *amount* of cash into *name* account."""
-        self.balances[name] = self.balances[name] + amount
+        """Deposit *amount* of cash into current account of *name* user."""
+        self.client_balances[name] = self.client_balances[name] + amount
 
     def raw_transfer(self, from_user, to_user, amount):
-        """Transfer money between users."""
-        # Are there any constraints?
-        self.balances[from_user] = self.balances[from_user] - amount
-        self.balances[to_user] = self.balances[to_user] + amount
+        """Transfer money between user accounts."""
+        # Should there be any constraints? (There are at least 2 to 4 constraints to consider).
+        self.client_balances[from_user] = self.client_balances[from_user] - amount
+        self.client_balances[to_user] = self.client_balances[to_user] + amount
+       
+
+class CurrentAccounts(CurrentAccountsBase):
+    def add_users(self, names):
+        """Write docstring here."""
+        # Write code here
 
     def transfer(self, from_user, to_user, amount):
         """Write docstring here."""
-        # Write code here
+        # Write code here using raw_trasfer()
+
+    def collect_fee(self, from_user, amount):
+        """Write off *amount* from a user and send money to bank own account."""
+        # Write code here usig trasfer and collect_fees()
 
     def transfer_with_fee(self, from_user, to_user, amount, fee):
         """Write docstring here."""
         # Write code here
 
-    def add_users(self, names):
-        """Write docstring here."""
-        # Write code here
-
-
-accounts = Accounts()
-accounts.create_user("rajiv")
-accounts.create_user("sara")
-accounts.create_user("jose")
-accounts.add_money("rajiv", 140)
-accounts.add_money("sara", 240)
-accounts.add_money("jose", 30)
-accounts.raw_transfer("jose", "rajiv", 10)
-accounts.raw_transfer(from_user="jose", to_user="sara", amount=10)
-print(accounts.balances)
-
-# API: 
-"user/create", ("rajiv")
-"account/topup", ("rajiv", 140)
-"account/transfer", ("jose", "sara", 10, 0.5)
-"accounts/"
+if __name__ == "__main__":
+    # Think of alternate constructor for CurrentAccoutns wit user names, and own_account
+    accounts = CurrentAccounts()
+    accounts.create_user("rajiv")
+    accounts.create_user("sara")
+    accounts.create_user("jose")
+    accounts.add_money("rajiv", 140)
+    accounts.add_money("sara", 240)
+    accounts.add_money("jose", 30)
+    accounts.raw_transfer("jose", "rajiv", 10)
+    accounts.raw_transfer(from_user="jose", to_user="sara", amount=10)
+    print(accounts.client_balances)
