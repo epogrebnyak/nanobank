b11527a31c8bedfb9be6ca1e423aa4b5ee97f2b4
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-02-11 09:00:35 +0300

    added API blueprint

--- payments/with_class.py
+++ payments/with_class.py
@@ -2,39 +2,46 @@ class Accounts:
     def __init__(self):
         self.balances = {"bank": 0}
 
-    def add_user(self, name):
-        # is this always correct to do?
+    def create_user(self, name):
+        # Is this always correct to do?
         self.balances[name] = 0
 
-    def deposit_cash(self, name, amount):
+    def add_money(self, name, amount):
         """Deposit *amount* of cash into *name* account."""
         self.balances[name] = self.balances[name] + amount
 
     def raw_transfer(self, from_user, to_user, amount):
         """Transfer money between users."""
+        # Are there any constraints?
         self.balances[from_user] = self.balances[from_user] - amount
         self.balances[to_user] = self.balances[to_user] + amount
 
     def transfer(self, from_user, to_user, amount):
         """Write docstring here."""
-        pass
+        # Write code here
 
     def transfer_with_fee(self, from_user, to_user, amount, fee):
         """Write docstring here."""
-        pass
+        # Write code here
 
     def add_users(self, names):
         """Write docstring here."""
-        pass
+        # Write code here
 
 
 accounts = Accounts()
-accounts.add_user("rajiv")
-accounts.add_user("sara")
-accounts.add_user("jose")
-accounts.deposit_cash("rajiv", 140)
-accounts.deposit_cash("sara", 240)
-accounts.deposit_cash("jose", 30)
+accounts.create_user("rajiv")
+accounts.create_user("sara")
+accounts.create_user("jose")
+accounts.add_money("rajiv", 140)
+accounts.add_money("sara", 240)
+accounts.add_money("jose", 30)
 accounts.raw_transfer("jose", "rajiv", 10)
 accounts.raw_transfer(from_user="jose", to_user="sara", amount=10)
 print(accounts.balances)
+
+# API: 
+"user/create", ("rajiv")
+"account/topup", ("rajiv", 140)
+"account/transfer", ("jose", "sara", 10, 0.5)
+"accounts/"
--- payments/with_functions.py
+++ payments/with_functions.py
@@ -1,19 +1,23 @@
 def create_balances():
+    """Write docstring here."""
     return dict(bank=0)
 
 
-def add_user(balances, name, amount=0):
+def create_user(balances, name, amount=0):
+    """Write docstring here."""
     balances[name] = amount
     return balances
 
 
-def deposit_cash(balances, name, amount):
+def add_money(balances, name, amount):
     """Deposit money into *name_to* account in cash or from other bank."""
     balances[name] = balances[name] + amount
     return balances
 
 
 def transfer(balances, from_user, to_user, amount):
+    """Write docstring here."""
+    # Is this always correct?
     balances[from_user] = balances[from_user] - amount
     balances[to_user] = balances[to_user] + amount
     return balances
@@ -23,10 +27,10 @@ def transfer(balances, from_user, to_user, amount):
 balances = create_balances()
 
 #  Creating users and balances
-balances = add_user(balances, "rajiv")
-balances = deposit_cash(balances, "rajiv", 40)
-balances = add_user(balances, "sara", 140)
-balances = add_user(balances, "jose", 30)
+balances = create_user(balances, "rajiv")
+balances = add_money(balances, "rajiv", 40)
+balances = create_user(balances, "sara", 140)
+balances = create_user(balances, "jose", 30)
 
 # Making two money transfers
 balances = transfer(balances, "jose", "rajiv", 10)
@@ -35,3 +39,16 @@ print(balances)
 
 # A test for our code
 assert balances == {"bank": 0, "rajiv": 50, "sara": 150, "jose": 10}
+
+# Problems:
+# 1. Overspending
+print(transfer(balances, "jose", "sara", 199))
+
+# 2. Not earning money
+print(balances["bank"])
+
+# 3. Cannot save the state of the accounts and read back
+
+# 4. Cannot prove correctness - no tests
+
+# 5. There are situations where the program will crash or misbehave.
