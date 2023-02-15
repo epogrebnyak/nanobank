2d663dbe62357701e0d9b4a746759e309bdbc12a
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-02-15 12:37:08 +0300

    add development ideas

--- dialogue/dialogue.py
+++ dialogue/dialogue.py
@@ -5,14 +5,6 @@ https://stackoverflow.com/questions/63121722/financial-calculator-bond-interest-
 """
 
 
-def compound_interest(annual_rate, years):
-    return (1 + annual_rate) ** years
-
-
-def simple_interest(annual_rate, years):
-    return 1 + annual_rate * years
-
-
 def get_text(message):
     return input(message + "\n> ")
 
@@ -23,9 +15,9 @@ def get_number(message):
 
 def get_selection(message, options):
     while True:
-        ans = get_text(message)
-        if ans in options:
-            return ans
+        answer = get_text(message)
+        if answer in options:
+            return answer
         else:
             print("Please enter any of:", " ".join(options))
 
@@ -35,13 +27,42 @@ p = get_number("How much are you depositing?")
 r = get_number("At which annual interest rate (in percent)?")
 t = get_number("How many years are you planning to invest for?")
 ans = get_selection("[c]ompound or [f]lat rate?", ["c", "f"])
+currency = "R" # this is a money symbol for South African rand https://en.wikipedia.org/wiki/South_African_rand
 
 # all of the calculation logic here in one place
+def compound_interest(annual_rate, years):
+    return (1 + annual_rate) ** years
+
+
+def simple_interest(annual_rate, years):
+    return 1 + annual_rate * years
+
+# a dictionary can help choosing the right function
 interest_func = dict(c=compound_interest, f=simple_interest)[ans]
 fv = p * interest_func(annual_rate=r / 100, years=t)
+
 # output
-currency = "R"
 print(
     f"Future value of your initial investment of {currency}{p} "
     + f"over {t} years under {r}% interest rate will be {currency}{fv:.2f}"
 )
+
+
+# IDEA: print the interest rate number to screen (as 2.25%) and ask about correctness 
+#       the source of error may be if a user enters 2.25 as 0.0225 
+#       (for example thinking a fraction is expected as interest rate)
+#       you will need a loop that would iterate if the number is not correct
+
+# IDEA: print("Please enter any of:", " ".join(options)) lists options as a b c
+#       you may want to write a, b or c
+
+# IDEA: another option to design a prompt is to suggest options 
+#       enter c for compund rate, f for flat rate. your expected 
+#       option parameter would need to change to a dicitonary. 
+
+# MORE: add dates (today and in the future), suggest a contract with lower or higher interest rate
+#       use a floating interest rate depending on inflation of central bank prime rate,
+#       save data to file, create an API for this deposit product, think of incentive 
+#       schemes for deposit providers and rebate a deposit provider with a small cashback.
+#       print some ASCII art - for example Decimal Dan from banner.txt 
+#       (generated form http://gordlaws.co.za/gcs-decimal-dan/ using https://www.ascii-art-generator.org/)
